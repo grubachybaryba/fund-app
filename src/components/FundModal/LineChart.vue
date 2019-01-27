b28c3f028032c2a483ad9b1edc4bb2c076592762
grubachybaryba <grubachybaryba@gmail.com>
2019-01-27 20:28:29 +0100

    feat(component): set line chart

--- src/components/FundModal/LineChart.vue
+++ src/components/FundModal/LineChart.vue
@@ -1,10 +1,53 @@
-<template>  
-    <div>Line Chart</div>
+<template>
+  <div>
+    <highcharts :options="chartOptions"></highcharts>
+  </div>
 </template>
 
 <script>
+import {Chart} from 'highcharts-vue'
+
 export default {
-  name: 'LineChart'  
+  name: 'LineChart',
+  components: {
+    highcharts: Chart 
+  },
+  props: {
+    performance: {
+      type: Array,
+      required: true
+    }
+  },  
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'spline'
+        },
+        title: {
+          text: null
+        },
+        legend: {
+          enabled: false,
+        },
+        series: [{}]
+      }
+    }
+  },
+  methods: {
+    processData(array) {
+      const processedData = array.map(obj => Object.values(obj).reverse())      
+      this.chartOptions.series = [{ data: processedData }]
+    }
+  },
+  watch: { 
+    performance: function(newVal) { 
+      this.processData(newVal)
+    }
+  },
+  created() {
+    this.processData(this.performance)
+  }  
 }
 </script>
 
