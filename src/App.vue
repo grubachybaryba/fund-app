9b968a17c4bfe214148c5b9352e6a0f1d09a4c9e
grubachybaryba <grubachybaryba@gmail.com>
2019-01-28 00:21:35 +0100

    feat(styles): set custom font

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 @import './assets/helpers.scss';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Exo', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
