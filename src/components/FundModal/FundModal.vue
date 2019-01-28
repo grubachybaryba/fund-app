b119d645ec202039942ffcf67eeee3885b98c6df
grubachybaryba <grubachybaryba@gmail.com>
2019-01-28 12:17:19 +0100

    fix(styles): modal fixes

--- src/components/FundModal/FundModal.vue
+++ src/components/FundModal/FundModal.vue
@@ -70,11 +70,23 @@ export default {
       max-height: 100vh;
       overflow: auto;
       // margin: 0px auto;
-      padding: 20px 30px;
+      padding: $s-md;
       background-color: #fff;
       // border-radius: 2px;
       box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
       transition: all .3s ease;
+
+      @include media(tablet-portrait-up) {
+        padding: $s-lg;
+      }
+    }
+
+    h2 {
+      margin-bottom: $s-md;
+    }
+
+    &__chart {
+      margin: $s-md 0;
     }
 
     &__button {
